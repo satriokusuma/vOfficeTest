5ad5d45604d1e490a29c72aa405108bccbd1bec0
riodev <rio88.dev@gmail.com>
2022-02-14 05:23:38 +0700

    add recervation page

--- frontend/src/components/organisms/index.js
+++ frontend/src/components/organisms/index.js
@@ -2,7 +2,8 @@ import Header from "./Header";
 import Navbar from "./Navbar";
 import Section from "./Section";
 import Footer from "./Footer";
+import RecervationList from "./RecervationList";
 
 
 
-export { Header, Section, Footer, Navbar };
+export { Header, Section, Footer, Navbar, RecervationList };
--- frontend/src/pages/RecervationScreen.js
+++ frontend/src/pages/RecervationScreen.js
@@ -6,7 +6,8 @@ import { listRecervation } from '../hook/actions/recervationAction';
 import { tabTitle } from '../utils/generalFunction';
 
 
-import { Button, Calendar, LabelRecervation, Loader, ValueRecervation } from '../components/atoms';
+import { Loader } from '../components/atoms';
+import { RecervationList } from '../components/organisms';
 
 
 const RecervationScreen = () => {
@@ -35,55 +36,12 @@ const RecervationScreen = () => {
   return (
     <div>
       {loading ? <Loader /> : error ? <h1>{error}</h1> :
-        recervation.map((item) => (
-          <div key={item.id}>
-            <div className='py-4'>
-              <h1 className='text-xl text-red-950 font-bold'>Your Recervation Client</h1>
-            </div>
-            <div className="border border-gray-200 mb-6 mt-2"></div>
-            <div className='flex flex-col'>
-              <div className='overflow-hidden'>
-                <img src={item.room.image ? process.env.REACT_APP_URI_ASSETS + item.room.image : '/image/noimage.jpeg'} alt={item.room.roomName} className='w-full rounded-lg h-52 md:h-80' />
-              </div>
-              <div>
-                <div className='flex flex-col'>
-                  <h2 className=' text-red-950 font-semibold text-lg py-2'>Description</h2>
-                  <Calendar date={new Date(item.bookingDate)} />
-                  <div className='grid grid-cols-6 gap-2 py-4 items-center'>
-                    <LabelRecervation className={'col-span-2'} label={'Room Name'} />
-                    <ValueRecervation className={'col-span-4 capitalize'} value={item.room.roomName} />
-
-                    <LabelRecervation className={'col-span-2'} label={'Room Cost'} />
-                    <ValueRecervation className={'col-span-4'} value={`Rp.${item.room.costPerHour}`} />
-
-                    <LabelRecervation className={'col-span-2'} label={'Location'} />
-                    <ValueRecervation className={'col-span-4'} value={'Jakarta'} />
-
-                    <LabelRecervation className={'col-span-2'} label={'Name'} />
-                    <ValueRecervation className={'col-span-4 capitalize'} value={item.client.name} />
-
-                    <LabelRecervation className={'col-span-2'} label={'Email'} />
-                    <ValueRecervation className={'col-span-4'} value={item.client.email} />
-
-                    <LabelRecervation className={'col-span-2'} label={'Phone'} />
-                    <ValueRecervation className={'col-span-4'} value={item.client.phone} />
-
-                    <LabelRecervation className={'col-span-2'} label={'Credit'} />
-                    <ValueRecervation className={'col-span-4'} value={`Rp.${item.client.credit}`} />
-
-                    <LabelRecervation className={'col-span-2'} label={'Time'} />
-                    <ValueRecervation className={'col-span-4'} value={`${item.startTime} to ${item.endTime}`} />
-
-                    <LabelRecervation className={'col-span-2'} label={'Quota'} />
-                    <ValueRecervation className={'col-span-4'} value={`${item.quotaUsed} Person`} />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <Button className={'bg-pink-600 text-white hover:bg-red-950 transition-all text-lg font-bold rounded-lg w-full my-5'} label='Download' />
-            <div className="border border-gray-200 mb-4 mt-2"></div>
-          </div>
-        ))
+        <div className='py-4'>
+          <h1 className='text-xl text-red-950 font-bold py-4'>Your Recervation Client</h1>
+          {recervation.map((item) => (
+            <RecervationList item={item} key={item.id} />
+          ))}
+        </div>
       }
     </div>
   );
